32d285b03b3b434d6d6e1ff4e9e2a1f070b50042
Bachors <bachor.can@gmail.com>
2015-03-18 22:07:36 +0700

    Update jquery.prefix-input.js

--- js/jquery.prefix-input.js
+++ js/jquery.prefix-input.js
@@ -5,4 +5,4 @@
 * Updates will be posted to this site.
 ************************************************/
 
-$(".ibacor_fi").focus(function(){var ibacor_prefix=$(this).data("prefix"),ibacor_currentId=$(this).attr('id');ibacor_fi(ibacor_prefix,ibacor_currentId);return false});function ibacor_fi(ibacor_prefix,ibacor_input_id){$('#'+ibacor_input_id).keydown(function(){setTimeout(function(){var ibacor_input=$('#'+ibacor_input_id).val();if(ibacor_input.match(new RegExp(ibacor_prefix))){$('#'+ibacor_input_id).val(ibacor_input)}else{$('#'+ibacor_input_id).val(ibacor_prefix)}},50)})}
+$(".ibacor_fi").focus(function(){var ibacor_prefix=$(this).data("prefix"),ibacor_currentId=$(this).attr('id');ibacor_fi(ibacor_prefix,ibacor_currentId);return false});function ibacor_fi(ibacor_prefix,ibacor_input_id){$('#'+ibacor_input_id).keydown(function(){setTimeout(function(){var ibacor_input=$('#'+ibacor_input_id).val(),ibacor_jumlah=ibacor_prefix.length,ibacor_cek=ibacor_input.substring(0,ibacor_jumlah);if(ibacor_input.match(new RegExp(ibacor_prefix))&&ibacor_cek==ibacor_prefix){$('#'+ibacor_input_id).val(ibacor_input)}else{$('#'+ibacor_input_id).val(ibacor_prefix)}},50)})}
