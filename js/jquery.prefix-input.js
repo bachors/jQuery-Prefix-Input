fe156cdd39c056273ce33b3fbae4210ff3afdc8d
Bachors <bachor.can@gmail.com>
2015-03-22 05:17:55 +0700

    Update jquery.prefix-input.js

--- js/jquery.prefix-input.js
+++ js/jquery.prefix-input.js
@@ -5,4 +5,4 @@
 * Updates will be posted to this site.
 ************************************************/
 
-$(".ibacor_fi").focus(function(){var a=$(this).data("prefix"),ibacor_currentId=$(this).attr('id'),ibacor_val=$(this).val();if(ibacor_val==''){$(this).val(a)}ibacor_fi(a,ibacor_currentId);return false});function ibacor_fi(b,c){$('#'+c).keydown(function(){setTimeout(function(){var a=$('#'+c).val(),ibacor_jumlah=b.length,ibacor_cek=a.substring(0,ibacor_jumlah);if(a.match(new RegExp(b))&&ibacor_cek==b){$('#'+c).val(a)}else{$('#'+c).val(b)}},50)})}
+$(".ibacor_fi").focus(function(){var a=$(this).data("prefix"),ibacor_currentId=$(this).attr('id'),ibacor_val=$(this).val();if(ibacor_val==''){$(this).val(a)}ibacor_fi(a,ibacor_currentId);return false});function ibacor_fi(c,d){$('#'+d).keydown(function(b){setTimeout(function(){var a=$('#'+d).val(),ibacor_jumlah=c.length,ibacor_cek=a.substring(0,ibacor_jumlah);if(a.match(new RegExp(c))&&ibacor_cek==c){$('#'+d).val(a)}else{if(b.key=='Control'||b.key=='Backspace'||b.key=='Del'){$('#'+d).val(c)}else{$('#'+d).val(c+b.key)}}},50)})}
